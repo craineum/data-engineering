e120dc66e91233361668f619eb6fe2b8b6ca53e3
Kevin Craine <kcraine@silvercloudinc.com>
2014-05-04 12:21:24 -0400

    Add call to controller to store data, modify view message

--- app/controllers/subsidiary_sales_controller.rb
+++ app/controllers/subsidiary_sales_controller.rb
@@ -4,6 +4,12 @@ class SubsidiarySalesController < ApplicationController
   end
 
   def create
+    begin
+      SubsidiarySale.process_upload params["uploadSubsidiary"]
+      @message = "Success"
+    rescue
+      @message = "Fail"
+    end
   end
 
 end
