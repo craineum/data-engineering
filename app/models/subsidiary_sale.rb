7a419e5dbf814a8f0198f76a60e639ea8f9b50e6
Kevin Craine <kcraine@silvercloudinc.com>
2014-05-04 12:39:54 -0400

    Finish model refactor

--- app/models/subsidiary_sale.rb
+++ app/models/subsidiary_sale.rb
@@ -3,11 +3,17 @@ class SubsidiarySale < ActiveRecord::Base
   require 'csv'
 
   def self.process_upload(file)
+    total_gross = 0
     if file.respond_to? :path
       CSV.foreach file.path, col_sep: "\t", headers: true do |row|
-        create_from_csv_row row
+        total_gross += create_from_csv_row(row).total
       end
     end
+    total_gross
+  end
+
+  def total
+    item_price * purchase_count
   end
 
   private
